15a1eb30fe69b6e984e00ad9efa2ebcae723e869
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-16 20:02:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,8 @@
 def speak_to_grandma(string)
   if string == 'I LOVE YOU GRANDMA!'
     "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.
+  elsif string != string.upcase
+  "HUH?! SPEAK UP, SONNY!"
+  else string == string.upcase
+  "NO, NOT SINCE 1938!"
 end  
